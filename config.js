0bb75764f4b433baba12c7e0c1f71e38f8195202
Dave <badave@gmail.com>
2014-07-03 09:50:54 -0700

    Uses mongodb remotely unless you specify localhost

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ var environment = process.env['NODE_ENV'] || "development";
 var fs = require('fs');
 var path = require('path');
 
+
 var config = module.exports = {
   proc: 'APP',
   pid: process.pid,
@@ -13,13 +14,16 @@ config.title = "Pornodora";
 
 config.database = {
   "mongodbs": {
-    "primary": "mongodb://localhost:27017/test",
-    "secondary": "mongodb://localhost:27017/test"
+    "primary": process.env['MONGOLAB_URI'] || "mongodb://heroku_app26732105:189s1s2gucuno2cnigec3blca2@ds037698.mongolab.com:37698/heroku_app26732105",
   }
 };
 
 if (environment === "development") {
   config.title = "Classy Project";
+
+  if (process.env['LOCALDB']) {
+    config.database.mongodbs.primary = "mongodb://localhost:27017/classyproject";
+  }
 }
 
 module.exports = config;
