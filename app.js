45e4705f841f68e407675508709ab2fc27d99127
Dave <badave@gmail.com>
2014-07-02 23:07:17 -0700

    This was a mistake

--- app.js
+++ app.js
@@ -3,25 +3,47 @@ var port = process.env['PORT'] || 8888;
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var compression = require('compression');
 
-var config = require('./config');
+
+var _ = global._ = require('underscore');
+var config = global.config = require('./config');
+var Bookshelf = global.Bookshelf = require('./bookshelf');
+var Bootie = global.Bootie = require('bootie');
+
+var VideoController = require('./controllers/video');
 
 app.engine('jade', require('jade').__express);
 app.set('view engine', 'jade');
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
+app.use(compression());
 
 app.locals = {
-    config: config
+  config: config
 };
 
+var router = new Bootie.Router({
+  version: "v1",
+  controllers: {
+    video: new VideoController({
+      db: Bookshelf.knex
+    })
+  }
+});
+
+app.use(router.url, router);
+
 app.get('/', function(req, res) {
-    res.render("index");
+  res.render("index");
 });
 
 
 app.get('/register', function(req, res) {
-    res.render("register");
+  res.render("register");
 });
+
+console.log("Starting server on  port " + port);
 
 app.listen(port);
--- config.js
+++ config.js
@@ -9,5 +9,11 @@ if (environment === "development") {
   config.title = "Classy Project";
 }
 
+config.postgres = {
+  'brown': process.env['HEROKU_POSTGRESQL_BROWN_URL'] || "postgres://cqtiyunqokypgt:qL5IL8x-6qUZYLNBk-wKH3jdQh@ec2-54-204-24-202.compute-1.amazonaws.com:5432/d4lg9dvukip17n?ssl=true"
+};
+
+
+
 
 module.exports = config;
