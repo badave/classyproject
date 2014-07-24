ed196dfc977b4bb62b8c9f4539f3dd7ec2db379b
Dave <badave@gmail.com>
2014-07-24 10:35:20 -0700

    Gets user

--- public/js/views/home/home.js
+++ public/js/views/home/home.js
@@ -3,11 +3,14 @@ define(function(require) {
   var PlayerView = require('../video/player');
   var EditorView = require('../editor/editor');
   var LoginView = require('../user/login');
+  var APP = require('../../constants');
 
   // Acts as a layout for multiple backbone views
   return Woodhouse.View.extend({
     template: function(context) {
-      return jade.render('home', context);
+      return jade.render('home', _.extend(context, {
+        user: APP.user.toJSON()
+      }));
     },
     onRender: function() {
       this.$el.find('.player-container').html(new PlayerView({
@@ -16,16 +19,20 @@ define(function(require) {
       }).render().$el);
     },
     openEditorModal: function() {
-      this.$el.find('.editor').html(new EditorView({
-        collection: this.collection.clone(),
-        paging: this.collection.paging
-      }).render().$el);
+      if(APP.user) {
+        this.$el.find('.editor').html(new EditorView({
+          collection: this.collection.clone(),
+          paging: this.collection.paging
+        }).render().$el);
 
-      this.$el.find('.editor-modal').modal('show');
+        this.$el.find('.editor-modal').modal('show');
+      }
     },
     openLoginModal: function() {
-      this.$el.find('.login').html(new LoginView().render().$el);
-      this.$el.find('.login-modal').modal('show');
+      if(APP.user) {
+        this.$el.find('.login').html(new LoginView().render().$el);
+        this.$el.find('.login-modal').modal('show');
+      }
     },
 
   });
